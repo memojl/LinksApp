e9ac92831eb11c07f016f57f467c99e3491141cb
gjimenez <gjimenez@wikitek.com.mx>
2026-07-21 19:37:05 -0600

    ENV

--- src/app/core/constants.js
+++ src/app/core/constants.js
@@ -5,13 +5,16 @@ const { environments } = await envConfig();
 ========================== */
 export const app = document.querySelector("#app");
 export const body = document.getElementsByTagName("body")[0];
-export const name = environments.name;
-export const version = environments.version;
-export const entorno = environments.entorno;//(host == 'localhost:5173') ? 'Desarrollo' : environments.entorno;
-export const prefix = environments.prefix;
-export const KEY = environments.key;
+export const name = environments?.name;
+export const version = environments?.version;
+export const entorno = environments?.entorno;
+export const prefix = environments?.prefix;
+export const Api = environments?.api;
+export const KEY = environments?.key;
+export const FirebaseCfg = environments?.firebase;
+export const EmailjsCfg = environments?.emailjs;
 export const theme = 'links';
 export const proyect = 'LinksApp';
-export const typeDev = 'vite'; //vite or webpack
+export const typeDev = 'vite'; 
 export const hostDev = (typeDev == 'vite') ? 'localhost:5173' : 'localhost:9001';
-export const hostPre = 'localhost';//(typeDev == 'vite') ? '127.0.0.1:4173' : 'localhost';
+export const hostPre = 'localhost';
