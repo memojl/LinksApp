8058a99a2e28983f9f37fffde0cc589b9050ab91
gjimenez <gjimenez@wikitek.com.mx>
2026-07-21 19:37:31 -0600

    EXT

---
+++ ext/assets/environments-Ck5aRs7I.js
@@ -0,0 +1,1 @@
+var e={name:`LinksApp`,version:`1.0.1`,description:`LinksApp- Single Page Application`,start_url:`https://memojl.github.io/LinksApp/`,lang:`es-MX`,api:`https://api.midominio.com`,empresa:`Multiportal`,entorno:`produccion`,prefix:`links_`,key:``,firebase:{},emailjs:{PUBLIC_KEY:``,SERVICE_ID:``,TEMPLATE_ID:``}};export{e as environments};
